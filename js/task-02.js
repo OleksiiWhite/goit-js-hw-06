2cb9f8bbe8a595088a015606a4fd63e792bb80b6
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-06 19:04:43 +0300

    add task js 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientContainerEl = document.querySelector('#ingredients');
+
+const makeIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.classList.add('item');
+    itemEl.textContent = `${ingredient}`;
+
+    return itemEl;
+  });
+};
+
+const elements = makeIngredients(ingredients);
+ingredientContainerEl.append(...elements);
