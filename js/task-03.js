ae27a651b8f0bb96a844d91de55780391a16c7bc
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-06 20:56:15 +0300

    add js task 3

--- js/task-03.js
+++ js/task-03.js
@@ -17,8 +17,17 @@ const listRef = document.querySelector('.gallery');
 // console.log(listRef);
 const makeGalerryEL = image => {
   const { url, alt } = image;
-  return `<li class = "gallery-item"><img src = "${url}" alt = "${alt}" width = "150" height = "200"/> </li>`;
+  return `<li class = "gallery-item"><img src = "${url}" alt = "${alt}" width = "200" height = "150"/> </li>`;
 };
 const makeGallerryList = images.map(makeGalerryEL).join('');
 
 listRef.insertAdjacentHTML('beforeend', makeGallerryList);
+
+const justifyContent = 'justify-content';
+const listStyle = 'list-style';
+
+listRef.style.display = 'flex';
+listRef.style.listStyle = 'none';
+listRef.style.justifyContent = 'space-evenly';
+listRef.style.padding = '0px';
+listRef.style.margin = '0px';
