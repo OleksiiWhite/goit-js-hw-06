3f6fdf9fe2d88fd2cd46fd14e08775c7acdf6884
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-10 14:56:34 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === '' || password.value === '') {
+    return alert('Not all form fields are filled!');
+  }
+  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  e.currentTarget.removeEventListener();
+});
