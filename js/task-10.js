5500578a441cf93ccba2e0a14b437242f2349413
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-10 17:33:46 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// const boxesElements = document.querySelector('#boxes');
+// const amountEl = document.querySelector('#controls input');
+// const createBtn = document.querySelector('[data-create');
+// const clearBtn = document.querySelector('[data-destroy');
+
+// createBtn.addEventListener('click', createBoxes);
+
+// clearBtn.addEventListener('click', destroyBoxes);
+
+// amountEl.addEventListener('input', e => {
+//   e.currentTarget.value;
+// });
+// const markupTable = [];
+
+// function createBoxes() {
+//   for (let i = 1; i <= amountEl.value; i += 1) {
+//     const markupEl = `<div style="background-color: ${getRandomHexColor()}; width: ${
+//       i * 10
+//     }; height: ${i * 10}"></div>`;
+//     markupTable.push(markupEl);
+//   }
+// }
+
+// function destroyBoxes() {}
+// boxesElements.innerHTML = markupTable.join('');
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
