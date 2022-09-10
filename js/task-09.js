8081485b428ddc47b20fbb56faba0f01a63c5f1e
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-10 13:58:47 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,17 @@
+const refs = {
+  changeColorBtn: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
+  body: document.querySelector('body'),
+};
+
+refs.changeColorBtn.addEventListener('click', changColorBtnlick);
+
+function changColorBtnlick() {
+  const currentColor = getRandomHexColor();
+  refs.color.textContent = currentColor;
+  refs.body.style.backgroundColor = currentColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-<div class="widget">
-  <p>
-    Background color: <span class="color">-</span>
-  </p>
-  <button type="button" class="change-color">
-    Change color
-  </button>
-</div>;
